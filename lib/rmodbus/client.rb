a6dcbb2b8f358b3ec3e0d423b4cbeefe4074cf0b
flipback <flipback@d897fe7a-6cbe-49f8-b918-a1f64d2d1a4c>
2009-01-30 00:40:37 +0000

    Add retries for tcp connection and read, default of 10 retries, still 1 second timeout. Fix typos - changed read_discret_input to read_discrete_input and marked former as deprecated. Change SlaveDiviceError to SlaveDeviceError.

--- lib/rmodbus/client.rb
+++ lib/rmodbus/client.rb
@@ -88,6 +88,9 @@ module ModBus
   class Client
   
     include Errors
+    # Number of times to retry on connection and read timeouts
+    CONNECTION_RETRIES = 10
+    READ_RETRIES = 10
 
     # Read value *ncoils* coils starting with *addr*
     #
@@ -99,8 +102,14 @@ module ModBus
     # Read value *ncoils* discrete inputs starting with *addr*
     #
     # Return array of their values
-    def read_discret_inputs(addr, ncoils)
+    def read_discrete_inputs(addr, ncoils)
       query("\x2" + addr.to_bytes + ncoils.to_bytes).to_array_bit[0..ncoils-1]
+    end
+
+    # Deprecated version of read_discrete_inputs
+    def read_discret_inputs(addr, ncoils)
+      warn "[DEPRECATION] `read_discret_inputs` is deprecated.  Please use `read_discrete_inputs` instead."
+      read_discrete_inputs(addr, ncoils)
     end
 
     # Read value *nreg* holding registers starting with *addr*
@@ -188,9 +197,17 @@ module ModBus
     def query(pdu)    
       send_pdu(pdu)
 
-      timeout(1) do
-        pdu = read_pdu
+      tried = 0
+      begin
+        timeout(1, ModBusTimeout) do
+        pdu = read_pdu
+       end
+      rescue ModBusTimeout => err
+        tried += 1
+        retry unless tried >= READ_RETRIES
+        raise ModBusTimeout.new, 'Timed out during read attempt'
       end
+      pdu = read_pdu
     
       if pdu[0].to_i >= 0x80
         case pdu[1].to_i
@@ -205,11 +222,11 @@ module ModBus
           when 5
             raise Acknowledge.new, "The server has accepted the request and is processing it, but a long duration of time will be required to do so"
           when 6
-            raise SlaveDiviceBus.new, "The server is engaged in processing a long duration program command"
++            raise SlaveDeviceBus.new, "The server is engaged in processing a long duration program command"
           when 8
             raise MemoryParityError.new, "The extended file area failed to pass a consistency check"
           else
-            raise ModBusException.new, "Unknow error"
+            raise ModBusException.new, "Unknown error"
         end
       end
       pdu[2..-1]
@@ -220,6 +237,9 @@ module ModBus
     end
 
     def read_pdu
+    end
+
+    def close
     end
 
   end
