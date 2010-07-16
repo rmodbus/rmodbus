af8c0a7098ac385ffd64162a0b35c0cb9ef3688b
Aleksey <atimin@gmail.com>
2010-07-17 01:12:13 +0600

    Added spec for Group

---
+++ spec/process/group_spec.rb
@@ -0,0 +1,31 @@
+begin
+  require 'rubygems'
+rescue
+end
+require 'rmodbus'
+
+include ModBus::Process
+
+describe Group do
+  before do
+    @grp = Group.new("grp")
+  end
+
+  it "should have name" do
+    @grp.name.should == "grp"
+  end
+
+  it "should have children group" do
+    @grp.add_group("grp_1")
+	@grp.grp_1.class.should == Group
+	@grp.grp_1.name.should == "grp_1"
+  end
+
+  it "should have points" do
+    @grp.add_point("point_1")
+	@grp.point_1.class.should == Point
+	@grp.point_1.name.should == "point_1"
+  end
+
+end
+
