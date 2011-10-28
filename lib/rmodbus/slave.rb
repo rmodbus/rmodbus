0531388db7de924351b0dc49272df1e90398a617
Aleksey Timin <atimin@gmail.com>
2011-10-29 03:20:20 +0600

    Refactoring

--- lib/rmodbus/slave.rb
+++ lib/rmodbus/slave.rb
@@ -267,55 +267,60 @@ module ModBus
         raise ModBusException.new, "Unknown error"
       end
 
+      check_response_mismatch(request, response) if raise_exception_on_mismatch
+      response[2..-1]
+    end
+
+    private
+    def check_response_mismatch(request, response)
+      read_func = response.getbyte(0)
       data = response[2..-1]
-      if raise_exception_on_mismatch
-        #Mismatch functional code
-        send_func = request[0]
-        if read_func != send_func
-          msg = "Function code is mismatch (expected #{send_func}, got #{read_func})"
-        end
-
-        case read_func
-        when 1,2
-          bc = request.getword(3)/8 + 1
-          if data.size != bc
-            msg = "Byte count is mismatch (expected #{bc}, got #{data.size} bytes)"
-          end
-        when 3,4
-          rc = request.getword(3) 
-          if data.size/2 != rc
-            msg = "Register count is mismatch (expected #{rc}, got #{data.size/2} regs)"
-          end
-        when 5,6
-          exp_addr = request.getword(1)
-          got_addr = response.getword(1)
-          if exp_addr != got_addr
-            msg = "Address is mismatch (expected #{exp_addr}, got #{got_addr})"
-          end
-
-          exp_val = request.getword(3)
-          got_val = response.getword(3)
-          if exp_val != got_val
-            msg = "Value is mismatch (expected 0x#{exp_val.to_s(16)}, got 0x#{got_val.to_s(16)})"
-          end
-        when 15,16
-          exp_addr = request.getword(1)
-          got_addr = response.getword(1)
-          if exp_addr != got_addr
-            msg = "Address is mismatch (expected #{exp_addr}, got #{got_addr})"
-          end
-        
-          exp_quant = request.getword(3)
-          got_quant = response.getword(3)
-          if exp_quant != got_quant
-            msg = "Quantity is mismatch (expected #{exp_quant}, got #{got_quant})"
-          end
-        end
-
-        raise ResponseMismatch.new(msg, request, response) if msg
+      #Mismatch functional code
+      send_func = request[0]
+      if read_func != send_func
+        msg = "Function code is mismatch (expected #{send_func}, got #{read_func})"
       end
 
-      data
+      case read_func
+      when 1,2
+        bc = request.getword(3)/8 + 1
+        if data.size != bc
+          msg = "Byte count is mismatch (expected #{bc}, got #{data.size} bytes)"
+        end
+      when 3,4
+        rc = request.getword(3) 
+        if data.size/2 != rc
+          msg = "Register count is mismatch (expected #{rc}, got #{data.size/2} regs)"
+        end
+      when 5,6
+        exp_addr = request.getword(1)
+        got_addr = response.getword(1)
+        if exp_addr != got_addr
+          msg = "Address is mismatch (expected #{exp_addr}, got #{got_addr})"
+        end
+
+        exp_val = request.getword(3)
+        got_val = response.getword(3)
+        if exp_val != got_val
+          msg = "Value is mismatch (expected 0x#{exp_val.to_s(16)}, got 0x#{got_val.to_s(16)})"
+        end
+      when 15,16
+        exp_addr = request.getword(1)
+        got_addr = response.getword(1)
+        if exp_addr != got_addr
+          msg = "Address is mismatch (expected #{exp_addr}, got #{got_addr})"
+        end
+      
+        exp_quant = request.getword(3)
+        got_quant = response.getword(3)
+        if exp_quant != got_quant
+          msg = "Quantity is mismatch (expected #{exp_quant}, got #{got_quant})"
+        end
+      else
+        warn "Fuiction (#{read_func}) is not supported raising response mismatch"
+      end
+
+      raise ResponseMismatch.new(msg, request, response) if msg
     end
   end
 end
